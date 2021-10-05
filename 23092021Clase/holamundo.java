6d711b33bc56ab874b021aa71df35e958f99829d
AaronVelasco93 <aaronvelasco1993@outlook.com>
2021-10-05 16:01:27 -0500

    Ejemplo de programa en java echo desde la terminal

---
+++ 23092021Clase/holamundo.java
@@ -0,0 +1,17 @@
+/**
+*@author AaronVelasco
+*
+*
+*/
+
+// Clase principal
+public class holamundo{
+	
+	public static void main (String args[]){
+		
+		System.out.println("Hola mundo ");
+
+	}
+
+
+}
