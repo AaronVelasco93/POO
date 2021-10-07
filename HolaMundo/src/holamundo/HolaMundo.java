27e0f60218ed7e72195d8c0e4a2fd6c9a975f788
AaronVelasco93 <aaronvelasco1993@outlook.com>
2021-10-07 15:26:04 -0500

    Programa en Java - Hola mundo 05/OCT/2021

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo;
+
+/**
+ *
+ * @author Aaron
+ * Fecha: 05 de Octubre del 2021
+ */
+/*Clase principal*/
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        System.out.println("Hola mundo ");
+
+        System.out.println("Hola mundo ");
+        System.out.println("Hola mundo ");
+
+        //declaracion de una variable de tipo entero
+        //palabra reservada que es  = int "nombre de variable";  int "nombre de variable" ="asignar un valor"
+        int var=19;
+        int var2;
+        
+        var2=var;
+        
+        //ahora var2 = 19
+        
+           }
+
+}
